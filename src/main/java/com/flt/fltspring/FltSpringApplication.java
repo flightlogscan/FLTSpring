1af8b2d8d97c5c544fab93ee1aaed8153ab4d811
wilbur <wilbur777@protonmail.com>
2024-02-03 12:15:38 -0700

    Add gitignore and intialize firebase

--- src/main/java/com/flt/fltspring/FltSpringApplication.java
+++ src/main/java/com/flt/fltspring/FltSpringApplication.java
@@ -1,15 +1,29 @@
 package com.flt.fltspring;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
 
 @SpringBootApplication
 @ServletComponentScan
 public class FltSpringApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        Resource resource = new ClassPathResource("credentials/flightlogtracer-43695-firebase-adminsdk-1dyid-e1c32007d0.json");
+
+        @SuppressWarnings("deprecation")
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(resource.getInputStream()))
+                .build();
+        FirebaseApp.initializeApp(options);
         SpringApplication.run(FltSpringApplication.class, args);
     }
-
 }
--- src/main/java/com/flt/fltspring/ImageAnalyzerDummyRestController.java
+++ src/main/java/com/flt/fltspring/ImageAnalyzerDummyRestController.java
@@ -1,34 +1,29 @@
 package com.flt.fltspring;
 
-import com.azure.ai.formrecognizer.documentanalysis.DocumentAnalysisClient;
-import com.azure.ai.formrecognizer.documentanalysis.DocumentAnalysisClientBuilder;
-import com.azure.ai.formrecognizer.documentanalysis.models.AnalyzeResult;
-import com.azure.ai.formrecognizer.documentanalysis.models.DocumentTable;
-import com.azure.ai.formrecognizer.documentanalysis.models.OperationResult;
-import com.azure.ai.formrecognizer.documentanalysis.models.Point;
-import com.azure.core.credential.AzureKeyCredential;
-import com.azure.core.util.BinaryData;
-import com.azure.core.util.polling.SyncPoller;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.flt.fltspring.model.AnalyzeImageResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.io.FileUtils;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Enumeration;
 
 @RestController
 public class ImageAnalyzerDummyRestController {
 
     @RequestMapping(method = RequestMethod.POST, path = "/api/analyze/dummy")
     public ResponseEntity<String> submitAnalyzeImageDummy(final HttpServletRequest request) {
+
+        System.out.println("method: " +request.getMethod());
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while(headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            System.out.println("Header Name - " + headerName + ", Value - " + request.getHeader(headerName));
+        }
 
         File file = new File("dummyResponse.txt");
         String data = "";
